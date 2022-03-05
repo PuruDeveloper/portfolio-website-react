21ae563b9cb0eee7f5ac4e07e6720a04d3ed930f
PuruDeveloper <78719151+PuruDeveloper@users.noreply.github.com>
2022-03-05 10:56:49 +0530

    Changed color combination for website.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import "./styles/App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import Projects from "./pages/Projects";
-// import Skills from "./pages/Skills";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
+import "./App.css";
+
 import Home from "./Component/Home";
 import About from "./Component/About";
 import Projects from "./Component/Projects";
